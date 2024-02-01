4aa65b0326f45fd10ee6ef42e9b04f9a0ccf8860
RavyHollow <mancino.s@hotmail.it>
2024-02-01 16:51:35 +0100

    READMETEXT modificationssss

--- 00.INPUT/transformation_inputcsv_script.py
+++ 00.INPUT/transformation_inputcsv_script.py
@@ -10,7 +10,7 @@ from shapely.geometry import Point
 
 
 # Specifica il percorso del file Shapefile di inputt
-input_shapefile = "../00.INPUT/NATIVE_DATASET/03_GFLD_NATIVE/Landslidepoints_GFLD.shp"
+input_shapefile = "../00.INPUT/NATIVE_DATASET/02_GFLD_NATIVE/Landslidepoints_GFLD.shp"
 
 # Leggi il file Shapefile
 gdf = gpd.read_file(input_shapefile)
@@ -28,13 +28,35 @@ gdf['WKT_GEOM'] = gdf['geometry'].apply(lambda geom: f"POINT ({geom.xy[0][0]} {g
 # gdf = gdf.drop('geometry', axis=1)
 
 # Specifica il percorso del file CSV di output
-output_csv = "../00.INPUT/NATIVE_DATASET/03_GFLD_NATIVE/03_UGLC_NATIVE.csv"
+output_csv = "../00.INPUT/NATIVE_DATASET/02_GFLD_NATIVE/02_UGLC_NATIVE.csv"
 
 
 # Salva il GeoDataFrame come file CSV
 gdf.to_csv(output_csv, index=False)
 
 print("Conversione completata con successo.")
+
+#-----------------------------------------------------------------------------------------------------------------------
+# native dataframe: ITALICA
+#-----------------------------------------------------------------------------------------------------------------------
+# Conversion
+#-----------------------------------------------------------------------------------------------------------------------
+
+import geopandas as gpd
+
+# Leggi il file CSV originale con geometrie
+df_orig = gpd.read_file('00.INPUT/NATIVE_DATASET/03_ITALICA_NATIVE/ITALICA-v2.csv')
+
+# Aggiungi una colonna con i valori WKT_GEOM
+df_orig['WKT_GEOM'] = df_orig.geometry.apply(lambda geom: geom.wkt)
+
+# Seleziona solo le colonne necessarie, includendo la nuova colonna WKT_GEOM
+df_output = df_orig[['WKT_GEOM', 'altra_colonna', 'ancora_una_colonna']]
+
+# Salva il nuovo DataFrame in un file CSV
+df_output.to_csv('00.INPUT/NATIVE_DATASET/03_ITALICA_NATIVE/03_ITALICA_NATIVE.csv', index=False)
+
+
 
 #-----------------------------------------------------------------------------------------------------------------------
 # native dataframe:
