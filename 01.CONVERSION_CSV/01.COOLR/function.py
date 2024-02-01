7224e524f509b5eca461fc0e1ef580d929428a28
Anis <137087317+Anita2333@users.noreply.github.com>
2024-02-01 15:24:35 +0100

    REVISIONE CARTELLE

---
+++ 01.CONVERSION_CSV/01.COOLR/function.py
@@ -0,0 +1,79 @@
+import pandas as pd
+from shapely.wkt import loads
+from opencage.geocoder import OpenCageGeocode
+
+import pandas as pd
+#1
+
+def apply_country_corrections(df):
+    def get_country_name(wkt_geom):
+        geometry = loads(wkt_geom)
+        longitude, latitude = geometry.xy
+        geolocator = Nominatim(user_agent="country_lookup")
+        location = geolocator.reverse((latitude[0], longitude[0]), language='en')
+        country = location.raw.get('address', {}).get('country')
+        return country
+
+    # Indexes rows with 'ND' in the column 'COUNTRY'
+    nd_rows = df[df['COUNTRY'] == 'ND'].index
+
+    # Iterate on the lines and update the country name
+    for idx in nd_rows:
+        wkt_geom = df.at[idx, 'WKT_GEOM']
+
+        try:
+            country_name = get_country_name(wkt_geom)
+            df.at[idx, 'COUNTRY'] = country_name
+        except Exception as e:
+            print("________________________________________________________________________________________")
+            #print(f"Error during the coordinates extraction from the row {idx}: {e}")
+            #print("________________________________________________________________________________________")
+
+# -----------------------------------------------------------------------------------------------------------------------
+#2
+
+def apply_affidability_calculator(df):
+    # Converti la colonna 'ACCURACY' in numeri, trattando 'ND' come NaN
+    df['ACCURACY'] = pd.to_numeric(df['ACCURACY'], errors='coerce')
+
+    # Funzione di trasformazione per assegnare un valore da 1 a 10 alla colonna 'AFFIDABILITY'
+    def assign_affidability(row):
+        accuracy = row['ACCURACY']
+        start_date = pd.to_datetime(row['START DATE'])
+        end_date = pd.to_datetime(row['END DATE'])
+
+        if pd.notna(accuracy):
+            if accuracy <= 100:
+                if start_date == end_date:
+                    return 1
+                else:
+                    return 2
+            elif 100 < accuracy <= 250:
+                if start_date == end_date:
+                    return 3
+                else:
+                    return 4
+            elif 250 < accuracy <= 500:
+                if start_date == end_date:
+                    return 5
+                else:
+                    return 6
+            elif 500 < accuracy <= 1000:
+                if start_date == end_date:
+                    return 7
+                else:
+                    return 8
+            elif accuracy > 1000:
+                return 9
+        else:  # 'ND' case
+            return 10
+
+    # Applica la funzione di trasformazione alla colonna 'AFFIDABILITY'
+    df['AFFIDABILITY'] = df.apply(assign_affidability, axis=1)
+
+    # Riconverti la colonna 'ACCURACY' in stringhe, trasformando i NaN in 'ND'
+    df['ACCURACY'] = df['ACCURACY'].fillna('ND')
+
+    print("________________________________________________________________________________________")
+    print("Valori di 'AFFIDABILITY' assegnati con successo.")
+    print("________________________________________________________________________________________")
