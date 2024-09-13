da6f59ab7bd93faed5680245464f3356bd3895a8
RavyHollow <mancino.s@hotmail.it>
2024-09-13 11:20:20 +0200

    unifier fixed for producing a duplicates-free catalog

--- output/UGLC_unifier.py
+++ output/UGLC_unifier.py
@@ -18,32 +18,27 @@ print(f"Using root= {root}")
 
 # -----------------------------------------------------------------------
 
-# Directory contenente i file CSV standardizzati
 directory = f"{root}/output/converted_csv/"
 
-# Lista dei file CSV nella directory
+# List sll CSV files into the dir
 csv_files = [file for file in os.listdir(directory) if file.endswith('.csv')]
-
-# DataFrame iniziale vuoto
 df_combined = pd.DataFrame()
 
-# Leggi tutti i file CSV e uniscili in un unico DataFrame
+# Read CSV files from the list then merge em as a single dataframe
 for file in csv_files:
     file_path = os.path.join(directory, file)
-    df_temp = pd.read_csv(file_path, dtype={'VERSION': object})  # Imposta il tipo di dati della colonna "VERSION" come object (stringa)
+    df_temp = pd.read_csv(file_path, dtype={'VERSION': object})  # Set datatype of the col "VERSION as obj str (hotfix)
     df_combined = pd.concat([df_combined, df_temp], ignore_index=True)
 
-#-----------------------------------TESTING-----------------------------
-# Rimuovi i duplicati basati su WKT_GEOM, START DATE, e END DATE
+# Removes duplicates based on WKT_GEOM, START DATE, END DATE (same events)
 df_combined.drop_duplicates(subset=['WKT_GEOM', 'START DATE', 'END DATE'], keep='first', inplace=True)
-#-----------------------------------------------------------------------
 
+# Generates ID
 df_combined['ID'] = [str(i) for i in range(1, len(df_combined) + 1)]
 
-# Verifica se ci sono file CSV nella directory specificata
+# Test file existence then save
 if csv_files:
-    # Salva il DataFrame combinato in un nuovo file CSV
-    output_file = f"{root}/output/UGLC_no_duplicates.csv" #EDITED FOR DROP DUPLICATES TESTING
+    output_file = f"{root}/output/UGLC_no_duplicates.csv"
     df_combined.to_csv(output_file, index=False, sep='|')
     print(f"UGLC dataset created on '{output_file}' path with '|' as separator.")
 else:
