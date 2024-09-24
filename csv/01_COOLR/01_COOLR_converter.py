6b1f89c70b86f98ec7a6a5e2d2f8d4534f244234
Saverio Mancino <152511017+RavyHollow@users.noreply.github.com>
2024-09-24 12:39:45 +0200

    Update 01_COOLR_converter.py

--- csv/01_COOLR/01_COOLR_converter.py
+++ csv/01_COOLR/01_COOLR_converter.py
@@ -42,6 +42,7 @@ df_OLD['loc_desc'] = df_OLD['loc_desc'].fillna('ND')
 df_OLD['ev_desc'] = df_OLD['ev_desc'].fillna('ND')
 df_OLD['ev_dateS'] = df_OLD['ev_date'].fillna('1956/01/01')
 df_OLD['ev_dateE'] = df_OLD['ev_date'].fillna('2023/01/01')
+df_OLD['ctry_name'] = df_OLD['ctry_name'].fillna('ND')
 
 # Application of lookup Tables to the columns of the old DataFrame
 for column in df_OLD.columns:
@@ -83,14 +84,14 @@ new_data = {
 # New dataframe Creation
 df_NEW = pd.DataFrame(new_data)
 
-# New Dataframe Updating with the Old Dataframe columns content values
+# New Dataframe Updating with the Old Dataframe columns content values.
 df_NEW['WKT_GEOM'] = df_OLD['WKT_GEOM']
 df_NEW['NEW DATASET'] = "UGLC"
 df_NEW['ID'] = "CALC"  #range(1, len(df_OLD) + 1)
 df_NEW['OLD DATASET'] = "Cooperative Open Online Landslide Repository - NASA"
 df_NEW['OLD ID'] = df_OLD['ev_id']
 df_NEW['VERSION'] = str("2019")
-df_NEW['COUNTRY'] = df_OLD['ctry_name'].fillna('ND')
+df_NEW['COUNTRY'] = df_OLD['ctry_name'].replace("CÃ´te d'Ivoire", "Côte d'Ivoire", inplace=True)
 df_NEW['ACCURACY'] = df_OLD['loc_acc']
 df_NEW['START DATE'] = df_OLD.apply(lambda row: pd.to_datetime(row['ev_dateS']).strftime('%Y/%m/%d'), axis=1)
 df_NEW['END DATE'] = df_OLD.apply(lambda row: pd.to_datetime(row['ev_dateE']).strftime('%Y/%m/%d'), axis=1)
@@ -121,6 +122,4 @@ print("_________________________________________________________________________
 print("                             01_COOLR_native conversion: DONE                           ")
 print("________________________________________________________________________________________")
 
-#-----------------------------------------------------------------------------------------------------------------------
-# End
 #-----------------------------------------------------------------------------------------------------------------------
